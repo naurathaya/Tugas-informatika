995a43c1e8a67591efd7c3debde8a75c85bfaa70
naurathaya <naurathayaz@gmail.com>
2025-05-27 11:02:39 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
 st.title("🐈‍⬛ naura's memories")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.image("kyan (ya)00676.jpg")
